a1a9ed975877f8aa6bbf30eac6d758298e542a58
Phil-Barv <56893671+Phil-Barv@users.noreply.github.com>
2022-12-15 15:44:02 -0300

    fix prod bug 2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import * as faceapi from 'face-api.js';
+import * as faceapi from '../face-api.min.js';
 import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css'
 
